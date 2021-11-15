2d2ea8510acf7b84f4e2413da203d80186821138
Miguel Vásquez <angelzizu06@gmail.com>
2021-11-15 09:31:25 -0500

    Deploy 1.5

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,12 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      external: ["react", "react-dom", "vite-plugin-pwa"],
+      external: [
+        "react",
+        "react-dom",
+        "@apollo/client/core",
+        "@apollo/client/link/error",
+      ],
     },
   },
   optimizeDeps: {
