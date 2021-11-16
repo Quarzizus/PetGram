24984ef100cd51b821753a756d8fc5d39eb55e2d
Miguel Vásquez <angelzizu06@gmail.com>
2021-11-16 10:10:14 -0500

    ADD-> PWA

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "PetGram",
+        short_name: "PetGram",
+        description: "The animals more fantastics of internet",
+        theme_color: "#FFFFFF",
+        icons: [
+          {
+            src: "favivon.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+          {
+            src: "favivon.svg",
+            sizes: "512x512",
+            type: "image/svg",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
-
-// VitePWA({
-//   includeAssets: ["favicon.svg"],
-//   manifest: {
-//     name: "PetGram",
-//     short_name: "PetGram",
-//     description: "The animals more fantastics of internet",
-//     theme_color: "#FFFFFF",
-//     icons: [
-//       {
-//         src: "favivon.svg",
-//         sizes: "512x512",
-//         type: "image/svg",
-//       },
-//       {
-//         src: "favivon.svg",
-//         sizes: "512x512",
-//         type: "image/svg",
-//         purpose: "any maskable",
-//       },
-//     ],
-//   },
-// }),
