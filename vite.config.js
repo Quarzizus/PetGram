e98d5759c8a975183b6fed958187dc4b07bb730e
Miguel Vásquez <angelzizu06@gmail.com>
2021-11-16 09:56:14 -0500

    Delete -> extensions

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".mjs", ".es", ".es6", ".js", ".jsx", ".ts", ".tsx", ".json"],
-  },
 });
 
 // VitePWA({
