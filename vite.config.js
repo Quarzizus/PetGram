b39e2976771cde064d5a18e5baf4a171fd50ede2
Miguel Vásquez <angelzizu06@gmail.com>
2021-11-16 09:35:29 -0500

    ADD-> Without PWA

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      includeAssets: ["favicon.svg"],
-      manifest: {
-        name: "PetGram",
-        short_name: "PetGram",
-        description: "The animals more fantastics of internet",
-        theme_color: "#FFFFFF",
-        icons: [
-          {
-            src: "favivon.svg",
-            sizes: "512x512",
-            type: "image/svg",
-          },
-          {
-            src: "favivon.svg",
-            sizes: "512x512",
-            type: "image/svg",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "@apollo/client/core",
-        "@apollo/client/link/error",
-      ],
-    },
-  },
-  optimizeDeps: {
-    include: [
-      "@apollo/client/core",
-      "@apollo/client/link/error",
-      "react-icons",
-    ],
-  },
+  plugins: [react()],
 });
+
+// VitePWA({
+//   includeAssets: ["favicon.svg"],
+//   manifest: {
+//     name: "PetGram",
+//     short_name: "PetGram",
+//     description: "The animals more fantastics of internet",
+//     theme_color: "#FFFFFF",
+//     icons: [
+//       {
+//         src: "favivon.svg",
+//         sizes: "512x512",
+//         type: "image/svg",
+//       },
+//       {
+//         src: "favivon.svg",
+//         sizes: "512x512",
+//         type: "image/svg",
+//         purpose: "any maskable",
+//       },
+//     ],
+//   },
+// }),
