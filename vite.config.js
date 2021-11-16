fb8f87ddb3a638e6f6cd610c13706549c54d6da1
Miguel Vásquez <angelzizu06@gmail.com>
2021-11-16 09:44:29 -0500

    ADD-> Graphql Plugin Vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // import { VitePWA } from "vite-plugin-pwa";
+import graphql from "@rollup/plugin-graphql";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), graphql()],
 });
 
 // VitePWA({
